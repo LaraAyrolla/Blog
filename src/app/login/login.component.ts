04e414dd7ba8624d4c88f105100b9f1c84346ec1
Lara Ayrolla <lara.laabreu@gmail.com>
2023-09-16 19:20:23 -0300

    feat: adding new visualization for when the user has to posts and handling expirable access tokens

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inject }  from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -13,21 +14,46 @@ export class LoginComponent {
   access_token: any;
   constructor(@Inject(DOCUMENT) document: Document, private http: HttpClient, private route: ActivatedRoute, private router: Router) {}
 
-  submitLogin()
-  {
+  ngOnInit(): void {
     this.access_token = localStorage.getItem('access_token') ?? '';
 
     if (this.access_token !== '') {
-      this.router.navigate(['posts']);
+      const url = 'http://localhost/api/auth/me';
+      const headers = {
+        'Authorization': 'Bearer ' + this.access_token
+      }
+      const options = {                                                                                                                                                                                 
+        headers: new HttpHeaders(headers), 
+      };
+      
+      this.http
+        .get<any>(url, options)
+        .pipe(
+          catchError(err => {return this.router.navigate(['login'])})
+        )
+        .subscribe(res => {
+          if (res !== null) {
+            this.router.navigate(['posts']);
+          }
+        })
+      ;
+
       return;
     }
+  }
 
+  submitLogin()
+  {
     const email = (<HTMLInputElement>document.getElementById("email")).value;
     const password = (<HTMLInputElement>document.getElementById("password")).value;
     let body = {email: email, password: password};
 
     const url = 'http://localhost/api/auth/login';
-    this.http.post<any>(url, body).subscribe(res => {
+    this.http.post<any>(url, body)
+    .pipe(
+      catchError(err => {return this.router.navigate(['login'])})
+    )
+    .subscribe(res => {
       this.access_token = res.access_token
 
       if (this.access_token !== '') {
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation} from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inject }  from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class PostsComponent {
   access_token: any;
@@ -15,22 +17,33 @@ export class PostsComponent {
   constructor(@Inject(DOCUMENT) document: Document, private http: HttpClient, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    let access_token = localStorage.getItem('access_token') ?? '';
+    this.access_token = localStorage.getItem('access_token') ?? '';
 
-    if (access_token !== ''){
-        const url = 'http://localhost/api/post';
-        const headers = {
-          'Authorization': 'Bearer ' + this.access_token,
-        }
-        const options = {                                                                                                                                                                                 
-          headers: new HttpHeaders(headers), 
-        };
+    if (this.access_token !== '') {
+      const url = 'http://localhost/api/post';
+      const headers = {
+        'Authorization': 'Bearer ' + this.access_token
+      }
+      const options = {                                                                                                                                                                                 
+        headers: new HttpHeaders(headers), 
+      };
 
-        this.http.get<any>(url, options).subscribe(res => {
-          this.posts = res.data
-        });
+      this.http
+        .get<any>(url, options)
+        .pipe(
+          catchError( err => {return this.router.navigate(['login'])})
+        )
+        .subscribe(res => {
+          this.posts = res.data;
 
-        return;
+          if (!Array.isArray(this.posts)) {
+            this.router.navigate(['login']);
+          }
+
+        })
+      ;
+
+      return;
     }
 
     this.router.navigate(['login']);
